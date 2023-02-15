5c810e9b040d14e9176e636e94513092f49bb1bc
jmauwerb <jeroen.werbrouck@ugent.be>
2023-02-15 16:12:22 +0100

    publish

--- src/components/index.js
+++ src/components/index.js
@@ -1,55 +1,68 @@
 import React, { useState, useEffect } from "react";
-import Viewer from "./Viewer";
-import { Catalog } from 'consolid-daapi'
-import { ReferenceRegistry } from 'consolid-raapi'
-import { Session } from '@inrupt/solid-client-authn-browser'
+import {Button, FormGroup, FormControlLabel, Checkbox} from '@mui/material'
 
 const LBDviewer = (props) => {
   const { piral } = props
   const constants = piral.getData("CONSTANTS")
-  
-  const [models, setModels] = useState(["http://localhost:3000/engineer/f177466f-5929-445f-b2d7-ee19576c7d3a"])
+
+  const [images, setImages] = useState([])
   const [dataset, setDataset] = useState("")
   const [selection, setSelection] = useState([])
-  const [session, setSession] = useState(new Session())
   const [height, setHeight] = useState(600)
+
+  const [activeImages, setActiveImages] = useState([])
+
+  async function getDistributions() {
+    const project = piral.getData(constants.ACTIVE_PROJECT)
+
+    const results = await piral.getResourcesByContentType(project, "https://www.iana.org/assignments/media-types/image/jpeg")
+    setImages(results)
+  }
+  
+  piral.on('store-data', ({ name, value }) => {
+    if (name === constants.SELECTED_CONCEPTS) {
+      const sel = value.map(item => item.references.filter((ref) => images.map(m => m.dUrl).includes(ref.document))).flat().map(i => i.identifier)
+      setSelection(sel)
+    }
+  });
 
   async function onSelect(sel) {
     setSelection(sel)
-    const alias = await findAliases(models[0], sel[0], props.state[constants.ACTIVE_PROJECT], props.state[constants.REFERENCE_REGISTRY], session)
-    piral.setDataGlobal(constants.SELECTED_CONCEPTS, [alias])
+    piral.setDataGlobal(constants.SELECTED_REFERENCES, [{activeDocument: images[0].dUrl, identifier: sel[0]}])
   }
 
-  async function findAliases(activeDocument, selectedElement, projectUrl, referenceRegistryUrl, activeSession) {
-    const project = new Catalog(activeSession, projectUrl)
-    const refReg = new ReferenceRegistry(activeSession, referenceRegistryUrl, project)
-    const reference = await refReg.findConceptByIdentifier(activeDocument, selectedElement)
-    return reference
+  function setActive(data, dist) {
+    setActiveImages(prev => {
+      if (prev.includes(dist.dUrl)) {
+        return prev.filter(item => item != dist.dUrl)
+      } else {
+        return [...prev, dist.dUrl]
+      }
+    })
   }
 
-  function getCurrentSelection() {
-    if (props.state[constants.SELECTED_CONCEPTS]) {
-      return props.state[constants.SELECTED_CONCEPTS].map(item => item.references.filter((ref) => models.includes(ref.document))).flat().map(i => i.identifier)
-    } else {
-      return []
-    }
-  }
 
   return (
     <div>
-      {(models.length) ? (
+      <div>
+    <Button onClick={() => getDistributions()} >Get images in project</Button>
+      </div>
+      <FormGroup>
+      {images.map(image => {
+        return <FormControlLabel key={image.dUrl} control={<Checkbox onChange={(i) => setActive(i, image)}/>} label={image.dUrl} />
+      })}
+    </FormGroup>
+      {(activeImages.length) ? (
         <div>
-          <Viewer
-            height={height}
-            models={models}
-            projection={"perspective"}
-            onSelect={onSelect}
-            selection={getCurrentSelection()}
-          />
+          {activeImages.map(image => {
+            return <div>
+          <img key={image} src={image} alt="test image"/>
+          </div>
+          })}
         </div>
       ) : (
         <div>
-          <p style={{ paddingTop: "10%" }}>No glTF models selected </p>
+          <p style={{ paddingTop: "10%" }}>No images selected </p>
         </div>
       )}
     </div>
